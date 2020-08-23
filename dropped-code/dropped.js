9f365573876adb4e1fcdc74d8c64f2560bee3def
mahelhelou <mahelhelou@gmail.com>
2020-08-23 23:26:41 +0300

    Refactoring http requests, editing search paragraph & reorder servies

---
+++ dropped-code/dropped.js
@@ -0,0 +1,18 @@
+// Init scrollspy
+    $('body').scrollspy({ target: '#main-menu' })
+
+    // Apply smooth scroll
+    $("#main-menu a").on('click', function (event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function () {
+
+          window.location.hash = hash;
+        });
+      }
+    });
